47b0d1b98c61168d961667249054a59e89c5cf81
Jhoan Muñoz <pirrijhoan@gmail.com>
2026-06-04 20:08:21 -0500

    Env fix

--- firebase.ts
+++ firebase.ts
@@ -4,12 +4,12 @@ import type { FirebaseApp } from 'firebase/app'
 import type { Firestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA1j3mybOpG9gcINcts2Pk8veE_m3VCGJI',
-  authDomain: 'mproyecto2-e6d4e.firebaseapp.com',
-  projectId: 'mproyecto2-e6d4e',
-  storageBucket: 'mproyecto2-e6d4e.firebasestorage.app',
-  messagingSenderId: '881475675016',
-  appId: '1:881475675016:web:aa61a99fca124074a4903d'
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 }
 
 const firebase: FirebaseApp = initializeApp(firebaseConfig)
